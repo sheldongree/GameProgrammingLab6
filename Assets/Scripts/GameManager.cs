6c9194583cf235015165732549207e639ee3e35d
sheldongree <89657544+sheldongree@users.noreply.github.com>
2024-10-02 15:52:44 -0400

    completed lab 6

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Add this namespace
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private int score = 0;
+    public int targetScore = 4; // Score to reach before changing scenes
+
+    void Awake()
+    {
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Optional
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Modify this function
+    public void IncrementScore()
+    {
+        score++;
+        Debug.Log("Score: " + score);
+
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // Use your new scene's name
+    }
+
+}
